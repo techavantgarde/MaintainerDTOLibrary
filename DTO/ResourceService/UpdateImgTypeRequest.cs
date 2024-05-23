d67867856a03107ea2a8e52c6e8cb271fd0a3e6f
Keshav_Raj_Choudhary <116002283+keshavraj3582@users.noreply.github.com>
2024-05-23 15:27:37 +0530

    added maintainer dto and stored procedure created folders for constant and all create update delete insert for maintainer service also created api for all crud operations of imgType and docType

---
+++ DTO/ResourceService/UpdateImgTypeRequest.cs
@@ -0,0 +1,9 @@
+namespace MaintainerDTOLIBRARY.ResourceService
+{
+    public class UpdateImgTypeRequest
+    {
+        public string? ImgTypeID{get;set;}
+        public string? ImgType{get;set;}
+        public string? UpdatedBy{get;set;}
+    }
+}
