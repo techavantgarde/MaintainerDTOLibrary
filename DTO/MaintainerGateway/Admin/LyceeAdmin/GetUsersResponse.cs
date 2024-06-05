31859e5e903e99f8d7e9e22c7cf47efbb7ac3f47
saheranadaf <saheranadaf11@gmail.com>
2024-06-05 16:27:35 +0530

    created class GetUsersResponse

---
+++ DTO/MaintainerGateway/Admin/LyceeAdmin/GetUsersResponse.cs
@@ -0,0 +1,9 @@
+namespace DTO.MaintainerGateway.Admin.LyceeAdmin
+{
+    public class GetUsersResponse
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Role { get; set; }
+    }
+}
