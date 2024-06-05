db8ab208f4aa35a988f003409fa44252f73625be
saheranadaf <saheranadaf11@gmail.com>
2024-06-05 17:51:06 +0530

    added userid in GetUsersResponse

--- DTO/MaintainerGateway/Admin/LyceeAdmin/GetUsersResponse.cs
+++ DTO/MaintainerGateway/Admin/LyceeAdmin/GetUsersResponse.cs
@@ -2,6 +2,7 @@ namespace DTO.MaintainerGateway.Admin.LyceeAdmin
 {
     public class GetUsersResponse
     {
+        public required string UserId { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Role { get; set; }
