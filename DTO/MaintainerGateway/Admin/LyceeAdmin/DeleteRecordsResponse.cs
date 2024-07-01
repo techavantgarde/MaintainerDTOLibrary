aa5d4def0b95e667a08c09c97d5d46fa62f6a092
saheranadaf <saheranadaf11@gmail.com>
2024-07-01 13:27:26 +0530

    added insti id in delete record

--- DTO/MaintainerGateway/Admin/LyceeAdmin/DeleteRecordsResponse.cs
+++ DTO/MaintainerGateway/Admin/LyceeAdmin/DeleteRecordsResponse.cs
@@ -2,6 +2,7 @@ namespace DTO.MaintainerGateway.Admin.LyceeAdmin
 {
     public class DeleteRecordsResponse
     {
-        public required string UserID {get;set;}
+        public string? UserID {get; set;}
+        public string? InstitutionID { get; set;}
     }
 }
