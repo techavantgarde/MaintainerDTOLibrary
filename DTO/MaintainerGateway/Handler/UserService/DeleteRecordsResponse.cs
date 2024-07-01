0afd918a6c68a7988bdaaa6b0888786f99de6c36
saheranadaf <saheranadaf11@gmail.com>
2024-07-01 10:57:28 +0530

    modified

--- DTO/MaintainerGateway/Handler/UserService/DeleteRecordsResponse.cs
+++ DTO/MaintainerGateway/Handler/UserService/DeleteRecordsResponse.cs
@@ -2,6 +2,6 @@ namespace DTO.MaintainerGateway.Handler.UserService
 {
     public class DeleteRecordsResponse
     {
-        public required List<string> UserID {get;set;}
+        public required string UserID {get;set;}
     }
 }
